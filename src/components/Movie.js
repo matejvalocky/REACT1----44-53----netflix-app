8423ed3676d62fe20b5189392d579342b8c41fc0
Matej Valocký <matej.valocky@gmail.com>
2024-01-28 23:42:35 +0100

    48. React - Stylování stránky a filmů

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,11 @@ import data from "../data"
 
 
 const Movie = () => {
-    return <div>
+    return <div className="all-movies">
         {
             data.map((oneMovie) => {
                 const { id, image, title, age, tags, description } = oneMovie
-                return <div key={id}>
+                return <div className="one-movie" key={id}>
                     <img src={image} alt="" />
                     <h2>{title}</h2>
                     <p>{age}</p>
